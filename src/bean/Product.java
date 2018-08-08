7894bbd940956ea7940869d209ec27cc8c2a804b
Diego Fernandes <diegofernandes88@hotmail.com>
2018-08-07 22:11:28 -0300

    Implementação da classe Product

---
+++ src/bean/Product.java
@@ -0,0 +1,43 @@
+package bean;
+
+public class Product {
+
+	private String name;
+	private Double space;
+	private Double value;
+	
+	public Product() {
+		
+	}
+
+	public Product(String name, Double space, Double value) {
+		this.name = name;
+		this.space = space;
+		this.value = value;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Double getSpace() {
+		return space;
+	}
+
+	public void setSpace(Double space) {
+		this.space = space;
+	}
+
+	public Double getValue() {
+		return value;
+	}
+
+	public void setValue(Double value) {
+		this.value = value;
+	}
+	
+}
---
+++ src/executor/Execute.java
@@ -0,0 +1,11 @@
+package executor;
+
+public class Execute {
+
+	public static void main(String[] args) {
+
+		
+		
+	}
+
+}
